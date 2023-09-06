8a360a86932ab6b76182dd151bb243cc5d1cbe4f
이예진 <86579541+yeahdy@users.noreply.github.com>
2023-09-06 20:04:21 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 34.58 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -2,56 +2,59 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
-    Map<String,Integer> orderMap = new HashMap<>();
-    List<String> courseOrders = new ArrayList<>();
+    //dfs 풀이
+    HashMap<String,Integer> orderMap = new HashMap<>();
     public String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
-        //※가장 많이 뽑힌 코스요리를 선택하기 때문에 orders 배열의 정렬은 필요없음
-        //주문 알파벳순 정렬
-        int i=0;
-        for(String order : orders){
-            orders[i] = Stream.of(order.split(""))
+        List<String> orderCourse = new ArrayList<>();
+        
+        //문자열 원소 오름차순 정렬
+        for(int i=0; i<orders.length; i++){
+            orders[i]= Arrays
+                .stream(orders[i].split(""))
                 .sorted()
                 .collect(Collectors.joining());
-            i++;
         }
         
-        //모든 주문에서 하나의 코스요리 갯수에 대한 조합을 뽑는다.
-        for(int c : course){
-            for(String order : orders){
-                if(order.length() < c) continue;
-                String orderCase = "";
-                combination(order.toCharArray(), orderCase, 0, c);
+        //하나의 코스에 대한 모든 주문서를 뽑는다.
+        for(int cs : course){
+            for(String od : orders){
+                if(od.length() < cs) continue;
+                combination(od, "", 0, cs);
             }
-            //각 코스요리 갯수에서 가장 많이 뽑힌 요리 찾기!
-            //Map의 value중 max를 찾은 후, max value인 것의 key를 찾아서 List에 넣는다.
+            
+            //각 코스의 최다 주문구성을 뽑는다
             if(!orderMap.isEmpty()){
-                Integer MAX = Collections.max(orderMap.values());
-                for(Map.Entry<String, Integer> entry : orderMap.entrySet()){
-                    if(MAX > 1 && entry.getValue() == MAX){
-                        courseOrders.add(entry.getKey());
+                int MAX= Collections.max(orderMap.values());
+                for(String odKey : orderMap.keySet()){
+                    if(orderMap.get(odKey).equals(MAX) && MAX > 1){
+                        orderCourse.add(odKey);
                     }
                 }
                 orderMap.clear();
             }
         }
         
-        //사전 순으로 오름차순 정렬 + 원소 알파벳 오름차순 정렬
-        Collections.sort(courseOrders);
-        return courseOrders.stream().toArray(String[]::new);
+        Collections.sort(orderCourse);
+        return orderCourse.stream().toArray(String[]::new);
     }
     
-    //orderStr:뽑을 배열, orderCase:뽑은 문자열, idx:다음 시작점, r:뽑을 갯수
-    private void combination(char[] orderStr, String orderCase, int idx, int r){
-		if (r == 0) {
-            //System.out.println("case :: " + orderCase);
-            orderMap.put(orderCase, orderMap.getOrDefault(orderCase,0)+1);  //get 후 없을 경우 default값
+    private void combination(String order, String selectedOd, int index, int course){
+        if(course == 0){
+            //System.out.printf("\t단어조합: %s\n",selectedOd);
+            orderMap.put(selectedOd, orderMap.getOrDefault(selectedOd,0)+1);
             return;
-		}
+        }
         
-		for (int i = idx; i < orderStr.length; i++) {
-			combination(orderStr, orderCase + orderStr[i], i + 1, r - 1);
-            // System.out.println("다음 시작점: " + i + "다음 시작할 문자열: "+orderCase);
-		}
+        // 한번 for문을 반복할 때, 재귀 호출을 통해 for문의 조건까지 계속해서 i+1 을 한다.
+            // 따라서 'ABCFG'에서 2개를 뽑을 때
+            // 첫 반복으로 A를 뽑아 놓고, A와 함께 조합될 나머지 한개를 재귀 호출을 통해 계속 i+1을 하며 뽑는다.
+        for(int i=index; i<order.length(); i++){
+            //System.out.printf("i: %d, index: %d, course: %d, char: %s\n",i,index,course,order.charAt(i));
+            combination(order, selectedOd + order.charAt(i), i+1, course-1);            
+        }
     }
+    
 }
+
+
+

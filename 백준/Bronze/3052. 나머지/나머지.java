d68da1716d5dc9e91943a6c89b44a317b26171e3
이예진 <86579541+yeahdy@users.noreply.github.com>
2024-07-05 10:08:00 +0900

    나머지.java Update

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -2,18 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
- 
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		HashSet<Integer> hs = new HashSet<Integer>();
- 
-		for (int i = 0; i < 10; i++) {
-			hs.add(Integer.parseInt(br.readLine()) % 42);
-		//입력받은 값의 나머지 값을 add메소드를 통해 HashSet에 저장
-		}
-		br.close();
-		System.out.print(hs.size()); //HashSet의 값 출력
-	}
+    public static void main(String[] args) throws IOException {
+       System.out.println(Main.remainder());
+    }
+
+    static int remainder () throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> set = new HashSet<>();
+        for (int i= 0; i < 10; i++){
+            int value = Integer.parseInt(br.readLine());
+            set.add(value % 42);
+        }
+        br.close();
+        return set.size();
+    }
+
 }

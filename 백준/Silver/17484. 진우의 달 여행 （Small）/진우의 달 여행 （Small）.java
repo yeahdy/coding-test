6d838232fc1a339dc65ff2dce31922a42d749931
이예진 <86579541+yeahdy@users.noreply.github.com>
2024-07-12 17:01:05 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 112 ms, Memory: 14464 KB -BaekjoonHub
    DP 풀이로 변경

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
@@ -1,54 +1,66 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    private static int[][] maps;
-    private static int M, N;
-    private static int result;
+public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        maps = new int[N][M];
-        result = Integer.MAX_VALUE;
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                maps[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for(int i = 0; i < M; i++) {
-            for(int j = 0; j < 3; j++) {//처음에 시작할때 3방향으로 갈 수 있음
-                solve(0, i, maps[0][i], j);
-            }
-        }
-
-        System.out.println(result);
+        System.out.println(new Main().getFuelPrice());
     }
 
-    static int[][] dir = {{1, -1}, {1, 0}, {1, 1}};
+    int getFuelPrice () throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] earthAndMoon = br.readLine().split("\\s");
+        int N = Integer.parseInt(earthAndMoon[0]);
+        int M = Integer.parseInt(earthAndMoon[1]);
+        int[][] space = new int[N][M];
 
-    static void solve(int nowX, int nowY, int sum, int d) {
-        if(nowX == N-1) {//도착한 경우
-            result = Math.min(result, sum);
-            return;
+        //공간의 연료값 초기화
+        for(int i=0; i<N; i++){
+            String[] data = br.readLine().split("\\s");
+            for(int j=0; j<M; j++){
+                space[i][j] = Integer.parseInt(data[j]);
+            }
         }
 
-        for(int i = 0; i < 3; i++) {
-            if(i == d) continue;//같은 방향으로 두번 연속 움직임 x
-            int nextX = nowX + dir[i][0];
-            int nextY = nowY + dir[i][1];
-
-            if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
-
-            solve(nextX, nextY, sum + maps[nextX][nextY], i);
+        //NOTE 전에 움직인 방향으로 이동 불가 어떻게 판단하지?
+        int[][][] steps = new int[N][M][3];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                Arrays.fill(steps[i][j], Integer.MAX_VALUE);
+            }
         }
 
+        //첫번째 행 초기화
+        for(int j=0; j<M; j++){
+            steps[0][j][0] = space[0][j];
+            steps[0][j][1] = space[0][j];
+            steps[0][j][2] = space[0][j];
+        }
+
+        for(int i=1; i<N; i++){ //다음행의 연료값 계산
+            for(int j=0; j<M; j++){
+                if(j == 0){
+                    steps[i][j][0] = Math.min(steps[i-1][j+1][1],steps[i-1][j+1][2]) +space[i][j];
+                    steps[i][j][1] = steps[i-1][j][0]+space[i][j];
+                }else if(j == M-1){
+                    steps[i][j][2] = Math.min(steps[i-1][j-1][1],steps[i-1][j-1][0]) +space[i][j];
+                    steps[i][j][1] = steps[i-1][j][2]+space[i][j];
+                } else{
+                    steps[i][j][0] = Math.min(steps[i-1][j+1][1],steps[i-1][j+1][2]) +space[i][j];
+                    steps[i][j][1] = Math.min(steps[i-1][j][0],steps[i-1][j][2]) +space[i][j];
+                    steps[i][j][2] = Math.min(steps[i-1][j-1][1],steps[i-1][j-1][0]) +space[i][j];
+                }
+
+            }
+        }
+
+        //마지막행의 최소값 찾기
+        int minPrice = Integer.MAX_VALUE;
+        for(int j=0; j<M; j++){
+            for(int k=0; k<3; k++){
+                minPrice = Math.min(minPrice,steps[N-1][j][k]);
+            }
+        }
+        return minPrice;
     }
 
 }

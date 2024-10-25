016c45d9400c77dcf0038fd717be9ff7d49309a0
이예진 <86579541+yeahdy@users.noreply.github.com>
2024-10-25 18:06:12 +0900

    [Silver II] Title: 안녕, Time: 108 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.java
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -18,16 +18,16 @@ public class Main {
         final int MAX_STAMINA = 100;
         int[] dp = new int[MAX_STAMINA+1];   //dp[체력]= 최대 기쁨
         for(int i = 1; i < N+1; i++){
-            for(int stamina=MAX_STAMINA-1; stamina >= 0; stamina--) {   //뒤에서 부터 특정 체력에 대한 기쁨을 갱신하면서 반복
-                int total = exhaustion[i]+stamina;  //현재 체력과 체력소모를 더해서 현재 상태에서 가능한 상황을 고려
-                if (total < MAX_STAMINA) {
-                    dp[total] = Math.max(dp[total], dp[stamina] + happy[i]);    //"이미 갱신된 기쁨"과 "현재 체력에 대한 기쁨"을 비교하기
+            for(int stamina=MAX_STAMINA; stamina > 0; stamina--) {   //뒤에서 부터 특정 체력에 대한 기쁨을 갱신하면서 반복
+                int total = stamina-exhaustion[i];  //현재 체력과 체력소모를 더해서 현재 상태에서 가능한 상황을 고려
+                if (total > 0) {
+                    dp[stamina] = Math.max(dp[stamina], dp[total] + happy[i]);    //"이미 갱신된 기쁨"과 "현재 체력에 대한 기쁨"을 비교하기
                     //System.out.printf("dp[%d] = Math.max(dp[%d],dp[%d]+happyArr[%d])\n", total, total, stamina , i);
                     //System.out.printf("dp[%d] = Math.max(%d,%d)\n\n", total, dp[total],dp[stamina] + happy[i]);
                 }
             }
         }
 
-        System.out.println(dp[99]);
+        System.out.println(dp[MAX_STAMINA]);
     }
 }

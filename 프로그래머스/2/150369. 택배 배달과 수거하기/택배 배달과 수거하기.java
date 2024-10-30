b8acaff6f0760e706e2a44dbf3fced2be28c7407
이예진 <86579541+yeahdy@users.noreply.github.com>
2024-10-30 17:26:56 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 9.34 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+/**
+알고리즘: 그리디
+시간복잡도: 1 ≤ cap ≤ 50, 1 ≤ n ≤ 100,000
+아이디어:
+- 항상 cap 만큼 수거나 배달을 할건데, 선 배달 or 수거 / 후 cap 만큼 충전
+- 배달 or 수거 할게 있다면 배달 or 수거 요청하기
+- 배달 or 수거를 다 할때 까지 최대 용량 cap 만큼 더하기
+   - 이때 현재 위치까지 왔다갔다는 뜻으로 현재위치*2 왕복 계산하기
+   - 왔다간 이유는 한번 최대용량 만큼 충전하면 이후 남은 집들도 들리면서 배달 or 수거를 하기 때문
+
+잘못생각한 이유:
+마지막 집이 어딘지 찾는데 중점을 뒀기 때문에 많은 경우에 대해 하나씩 구현함..
+(가장 마지막 집에 수거나 배달을 할 게 있다면 들려야하기 때문에)
+*/
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        int d_wait = 0;
+        int p_wait = 0;
+        
+        for(int i = n-1 ; i >= 0 ; i--) {
+            d_wait += deliveries[i]; 
+            p_wait += pickups[i];
+            
+            while(d_wait > 0 || p_wait > 0) {
+                d_wait -= cap; 
+                p_wait -= cap;
+                answer += 2*(i+1); 
+            }
+        }
+        
+        
+        return answer;
+    }
+}

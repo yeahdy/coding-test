89dd6497b305d07598a913549e9b26a6255c0a47
이예진 <86579541+yeahdy@users.noreply.github.com>
2024-11-13 09:19:14 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,16 @@
+--  Python이나 C# 스킬을 가진 개발자의  ID, 이메일, 이름, 성을 조회
+SELECT 
+    d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+FROM DEVELOPERS d
+INNER JOIN SKILLCODES s
+ON d.SKILL_CODE & s.CODE > 0
+WHERE s.NAME IN ('Python','C#')
+GROUP BY d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+ORDER BY d.ID;
+
+# SELECT
+#     CODE
+#     ,CONV(CODE,10,2)
+#     ,CODE&8452
+# FROM SKILLCODES
+# WHERE NAME IN ('Python','C#');

4b7f68c77c44cf4408ce24d04ec4b5110f353a8d
이예진 <86579541+yeahdy@users.noreply.github.com>
2024-11-22 09:17:22 +0900

    [level 2] Title: 디펜스 게임, Time: 94.47 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/README.md
+++ 프로그래머스/2/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085#) 
 
 ### 성능 요약
 
-메모리: 142 MB, 시간: 92.91 ms
+메모리: 127 MB, 시간: 94.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 22일 01:40:15
+2024년 11월 22일 09:17:17
 
 ### 문제 설명
 
--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -4,24 +4,22 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
+
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int i=0; i<enemy.length; i++){
-            if(n < enemy[i] && k == 0){
+        for (int e : enemy) {
+            if (n < e && k == 0) {
                 break;
             }
-            
-            pq.offer(enemy[i]); //적 추가 
-            //System.out.printf("%d < enemy[%d] (%d)%n",n,i,enemy[i]);
-            //System.out.println(pq);
-            if(n < enemy[i]){   //현재 병사로 적 제거가 불가능할 경우
+
+            pq.offer(e);
+            if (n < e) {
                 n += pq.poll();
                 k--;
             }
-            n -= enemy[i];  //적 제거    
+            n -= e;
             answer++;
         }
-        
+
         return answer;
     }
 }

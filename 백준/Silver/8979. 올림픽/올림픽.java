c0fa72ec0a657ad8ec03e91144b3b28bff260b63
이예진 <86579541+yeahdy@users.noreply.github.com>
2024-12-07 01:12:53 +0900

    [Silver V] Title: 올림픽, Time: 116 ms, Memory: 14488 KB, Score: 45 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+//등수 중복 가능 차수 밀림
+//1 ≤ N ≤ 1,000
+//PriorityQueue 정렬
+public class Main {
+    static class Nation implements Comparable<Nation> {
+        int name;
+        int gold;
+        int silver;
+        int bronze;
+
+        public Nation(int name, int gold, int silver, int bronze) {
+            this.name = name;
+            this.gold = gold;
+            this.silver = silver;
+            this.bronze = bronze;
+        }
+
+        @Override
+        public int compareTo(Nation o) {
+            if(this.gold == o.gold){
+                if(this.silver == o.silver){
+                    return o.bronze - this.bronze;
+                }
+                return o.silver - this.silver;
+            }
+            return o.gold - this.gold;
+        }
+    }
+
+    static PriorityQueue<Nation> pq = new PriorityQueue<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            int nation = Integer.parseInt(st.nextToken());
+            int gold = Integer.parseInt(st.nextToken());
+            int silver = Integer.parseInt(st.nextToken());
+            int bronze = Integer.parseInt(st.nextToken());
+            pq.offer(new Nation(nation, gold, silver, bronze));
+        }
+
+        System.out.println(findRank(K));
+    }
+
+    static int findRank(int K){
+        int rank = 1;
+        Nation prev = pq.poll();
+        if(prev.name == K){
+            return rank;
+        }
+
+
+        while(!pq.isEmpty()){
+            int same = 0;
+            Nation nation = pq.poll();
+            //동점일 경우
+            if(prev.gold == nation.gold && prev.silver == nation.silver && prev.bronze == nation.bronze){
+                same++;
+            }else {
+                rank++;
+            }
+
+            if(nation.name == K){
+                break;
+            }
+
+            prev = nation;
+            rank += same;
+        }
+        return rank;
+    }
+}

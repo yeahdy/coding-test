c72414a4d6c9eb94a7daccece91d23f8fa666903
이예진 <86579541+yeahdy@users.noreply.github.com>
2024-12-12 21:58:43 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 300 ms, Memory: 26888 KB, Score: 66 point -BaekjoonHub

--- 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
@@ -16,20 +16,22 @@ public class Main {
 
         int start = 1;
         int end = 0;
-        int satisfaction =0;
+        int satisfaction =branch[0];
         long[] dp = new long[n+1];  // dp[현재지점] = 최대 누적 탈피 에너지
 
-        while (start <= n) {
-            satisfaction += branch[start];
-            dp[start] = dp[start - 1];
-            while (satisfaction >= k) {
-                dp[start] = Math.max(dp[start], dp[end] + satisfaction - k);
-                satisfaction -= branch[end + 1];
-                end++;
+        while(start < n+1){
+            if(satisfaction < k){
+                dp[start] = Math.max(dp[start],dp[start-1]);    //현재까지의 최대 누적에너지 갱신
+                if(start < n+1){
+                    satisfaction += branch[start++];    //k 보다 작으면 start 오른쪽으로 이동
+                }
+            }else{
+                while(satisfaction >= k){
+                    dp[start] = Math.max(dp[start], dp[end] + satisfaction - k);
+                    satisfaction -= branch[end++];  //end 오른쪽으로 이동 시켜서 범위 좁히기
+                }
             }
-            start++;
         }
-
         System.out.println(dp[n]);
     }
 }
